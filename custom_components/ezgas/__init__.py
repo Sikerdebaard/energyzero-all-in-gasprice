1e6f98194387cef7586dae9a4accd06d99e7e91b
T Phil <thomas@tphil.nl>
2024-05-24 18:03:46 +0200

    debug sensors

--- custom_components/ezgas/__init__.py
+++ custom_components/ezgas/__init__.py
@@ -1,15 +1,12 @@
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import discovery
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry):
-    hass.async_create_task(
-        discovery.async_load_platform(hass, "sensor", DOMAIN, {}, entry)
-    )
+    hass.async_add_job(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry):
-    return True
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
